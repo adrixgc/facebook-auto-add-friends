458ae64bca964b1d064a49b40ce6838328775505
Adrián García Campos <adrix91@hotmail.com>
2020-05-11 19:32:10 +0200

    Update fb-add-friends.js

--- fb-add-friends.js
+++ fb-add-friends.js
@@ -8,16 +8,24 @@
 // @grant        none
 // ==/UserScript==
 
+function sleepFor( sleepDuration ){
+    var now = new Date().getTime();
+    while(new Date().getTime() < now + sleepDuration){ /* do nothing */ } 
+};
+
 ! function() {
     "use strict";
     var e = 1;
     document.getElementById("fbSearchResultsBox").scrollIntoView(!0);
     var l = setInterval(function() {
             var e, l = document.querySelectorAll(".layerCancel");
-            for (e = 0; e < l.length; ++e) l[e].click()
+            for (e = 0; e < l.length; ++e){
+             l[e].click();
+             sleepFor((Math.round(Math.random() * 5) + 2)*1000);
+             }
         }, 100),
         t = setInterval(function() {
             var n = document.querySelectorAll(".FriendRequestAdd");
             e < n.length ? (n[e].scrollIntoView(!0), window.scrollBy(0, 96.5), n[e].click(), e++) : (clearInterval(t), clearInterval(l), window.location.reload(!1))
-        }, 1e3)
+        }, 10000)
 }();
